1177612e9f25da43a59bff3079e1484ed02c8629
polreig <polreig@alumni.uv.es>
2025-10-31 12:02:34 +0100

    Terminado limpieza de variables gastos

--- scripts/limpieza_gastos.R
+++ scripts/limpieza_gastos.R
@@ -70,14 +70,34 @@ datos <- datos %>%
 # Verificar los cambios
 colnames(datos)
 
-# Calcular porcentaje de NAs por columna
-porcentaje_nas <- colMeans(is.na(datos)) * 100
+# Identificar SI (1) estudiantes vs NO (0) estudiantes.
 
-# Crear un dataframe con los resultados
-analisis_nas <- data.frame(
-  variable = names(datos),
-  porcentaje_nas = porcentaje_nas,
-  n_nas = colSums(is.na(datos)),
-  n_total = nrow(datos)
-) %>%
-  arrange(desc(porcentaje_nas))
+table(datos$ESTUDIANTE)
+
+# Limpieza basada en los gastos totales de los estudiantes
+limpieza_gastos <- function(datos) {
+  
+  datos_limpios <- datos %>%
+    # Solo asegurar que tenemos estudiantes con gasto total conocido
+    filter(ESTUDIANTE == 1, !is.na(gasto_total_educacion)) %>%
+    
+    # Imputación solo para análisis específicos
+    mutate(
+      # Para servicios: NAs = 2 (No)
+      across(starts_with("servicio_"), ~ ifelse(is.na(.), 2, .)),
+      
+      # Importes: NAs = 0 (no gastaron en ese concepto)
+      across(starts_with("importe_"), ~ ifelse(is.na(.), 0, .)),
+      
+      # Gastos Totales: NAs = 0
+      across(starts_with("gasto_total_"), ~ ifelse(is.na(.), 0, .)),
+      
+      # Gastos en bienes: NAs = 2 (no gastaron)
+      across(starts_with("gasto_"), ~ ifelse(is.na(.), 2, .)),
+      
+    )
+  
+  return(datos_limpios)
+}
+
+dataset_limpio <- limpieza_gastos(datos)
