022bb676fe3763f0faff4d75e988a0720ef51ecc
polreig <polreig@alumni.uv.es>
2025-11-07 09:24:42 +0100

    arreglado limpieza_gastos y creado PartePol.Rmd

--- scripts/limpieza_gastos.R
+++ scripts/limpieza_gastos.R
@@ -1,3 +1,62 @@
+# Cambiar los nombres de las columnas usando dplyr
+datos <- datos %>%
+  rename(
+    
+    # Gastos en servicios educativos
+    importe_matricula_clases = MCL,
+    servicio_comedor = C08,
+    importe_comedor = C09A,
+    servicio_transporte = C10,
+    importe_transporte = C11A,
+    servicio_alojamiento = C12,
+    importe_alojamiento = C13A,
+    servicio_ampliacion_horario = C14,
+    importe_ampliacion_horario = C15A,
+    servicio_clases_apoyo = C16,
+    importe_clases_apoyo = C17A,
+    servicio_actividades_extraescolares = C18,
+    importe_actividades_extraescolares = C19A,
+    servicio_actividades_complementarias = C20,
+    importe_actividades_complementarias = C21A,
+    servicio_cuota_ampa = C22,
+    importe_cuota_ampa = C23A,
+    servicio_otros = C24,
+    importe_otros_servicios = C25A,
+    
+    # Clases particulares y estudios no reglados
+    servicio_clases_particulares = C26,
+    importe_clases_particulares = C27A,
+    importe_escuela_idiomas = C28A,
+    importe_estudios_idiomas_no_reglados = C29A,
+    importe_ensenanzas_artisticas = C31A,
+    importe_oposiciones_pruebas_acceso = C33A,
+    importe_otros_estudios = C34A,
+    
+    # Gastos en bienes
+    gasto_libros = DLIB,
+    importe_libros = LIB,
+    gasto_fotocopias = D42,
+    importe_fotocopias = D43,
+    gasto_uniformes = D44,
+    importe_uniformes = D45,
+    gasto_papeleria = D47,
+    importe_papeleria = D48,
+    gasto_muebles_accesorios = D50,
+    importe_muebles_accesorios = D51,
+    gasto_productos_informaticos = D53,
+    importe_productos_informaticos = D54,
+    gasto_otros_bienes = D56,
+    importe_otros_bienes = D57,
+    
+    # Totales de gasto
+    gasto_total_educacion = GTT,
+    gasto_total_servicios = GTS,
+    gasto_total_servicios_reglados = GTSR,
+    gasto_total_servicios_no_reglados = GTSNR,
+    gasto_total_bienes = GTB
+  )
+
+
 # Verificar los cambios
 colnames(datos)
 
